1c5dac040a64492ad4f8499024641b47da0431f9
Sean O'Brien <spoz6d@mst.edu>
2016-06-30 10:28:08 -0400

    Wrote Server.  Wrote most of CLientHandler

--- java-server/src/main/java/com/cooksys/ftd/assessment/filesharing/server/ClientHandler.java
+++ java-server/src/main/java/com/cooksys/ftd/assessment/filesharing/server/ClientHandler.java
@@ -1,5 +1,52 @@
 package com.cooksys.ftd.assessment.filesharing.server;
 
-public class ClientHandler {
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 
+import com.cooksys.ftd.assessment.filesharing.dao.*;
+
+public class ClientHandler implements Runnable {
+	
+	private BufferedReader reader;
+	private PrintWriter writer;
+	
+	private FileDao fileDao;
+	private UserDao userDao;
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+
+	public BufferedReader getReader() {
+		return reader;
+	}
+
+	public void setReader(BufferedReader reader) {
+		this.reader = reader;
+	}
+
+	public PrintWriter getWriter() {
+		return writer;
+	}
+
+	public void setWriter(PrintWriter writer) {
+		this.writer = writer;
+	}
+
+	public FileDao getFileDao() {
+		return fileDao;
+	}
+
+	public void setFileDao(FileDao fileDao) {
+		this.fileDao = fileDao;
+	}
+
+	public UserDao getUserDao() {
+		return userDao;
+	}
+
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
 }
