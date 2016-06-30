bf512709d8d62e1e696436419cd5a248a7ac8e52
Sean O'Brien <spoz6d@mst.edu>
2016-06-30 10:13:00 -0400

    Created skeleton files for server and client

---
+++ java-server/src/main/java/com/cooksys/ftd/assessment/filesharing/dao/FileDao.java
@@ -0,0 +1,20 @@
+package com.cooksys.ftd.assessment.filesharing.dao;
+
+import java.util.List;
+
+import com.cooksys.ftd.assessment.filesharing.model.db.UserFiles;
+
+public class FileDao extends AbstractDao {
+	
+	public UserFiles addFile() {
+		return null;
+	}
+	
+	public List<String> indexFile() {
+		return null;
+	}
+	
+	public UserFiles sendFile() {
+		return null;
+	}
+}
---
+++ java-server/src/main/java/com/cooksys/ftd/assessment/filesharing/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.cooksys.ftd.assessment.filesharing.dao;
+
+import java.util.Optional;
+
+import com.cooksys.ftd.assessment.filesharing.model.db.User;
+
+public class UserDao extends AbstractDao {
+	
+	public User createUser(User user) {
+		return null;
+	}
+	
+	public Optional<User> getUserByUsername (String username) {
+		return null;
+	}
+}
