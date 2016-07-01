a9c6d716f5800954d7644ae80faecee4d60e9068
Sean O'Brien <spoz6d@mst.edu>
2016-07-01 08:28:34 -0400

    Base implementation of CLI login

--- javascript-client/src/cli.js
+++ javascript-client/src/cli.js
@@ -1,4 +1,4 @@
-import fs from 'fs'
+// import fs from 'fs'
 import net from 'net'
 import vorpal from 'vorpal'
 import { hash, compare } from './hashing'
@@ -15,7 +15,6 @@ cli
     let server = net.createConnection({port: 667}, () => {
       let command = 'register'
       let hashed = hash(args.password)
-      fs.writeFile('output.json', JSON.stringify(`{ ClientMessage: { ${command}, content: ${args.username} ${hashed}}}\n`))
       server.write(JSON.stringify(`ClientMessage: {${command}, content: ${args.username} ${hashed}}\n`))
       this.log('Wrote to server')
       server.on('data', (data) => {
@@ -31,24 +30,39 @@ cli
     })
   })
 
-/* cli
+cli
   .mode('login <username> <password>')
   .description('Logs into server with given Username and Password')
-  .action()
+  .init(function (args, callback) {
+    let server = net.createConnection({port: 667}, () => {
+    })
 
-cli
-  .command('files')
-  .description('Retrieves a list of files stored on the server')
-  .action()
+    cli
+      .command('files')
+      .description('Retrieves a list of files stored on the server')
+      .action()
 
-cli
-  .command('upload <local file path> [path stored in database]')
-  .description('Uploads a new file to the server')
-  .action()
+    cli
+      .command('upload <local file path> [path stored in database]')
+      .description('Uploads a new file to the server')
+      .action()
 
-cli
-  .command('download <database file id> [local file path]')
-  .descrition('Retrieves the specified file from the server')
-  .action()*/
+    cli
+      .command('download <database file id> [local file path]')
+      .descrition('Retrieves the specified file from the server')
+      .action()
+
+    server.on('data', (data) => {
+      this.log(data.toString())
+    })
+
+    server.on('end', () => {
+      this.log('disconnected from server :(')
+      cli.exec('exit')
+    })
+  })
+  .action(function (command, callback) {
+    callback()
+  })
 
 export default cli
